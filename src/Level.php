c7276f7dfc9a2199c5365570331fc9962ad5244c
Eliel Ferreira <eliel_elie@hotmail.com>
2023-09-01 11:01:11 -0300

    Code refactored

--- src/Level.php
+++ src/Level.php
@@ -60,21 +60,31 @@ class Level
 
     public function getName(): string
     {
-        return match ($this->value) {
-            self::None => 'None',
-            default => ucfirst($this->value),
-        };
+        if ($this->value === "None") {
+            return "None";
+        } else {
+            return ucfirst($this->value);
+        }
     }
 
     public function getClass(): string
     {
-        return match ($this->value) {
-            self::Processed => 'success',
-            self::Debug, self::Info, self::Notice, self::Processing => 'info',
-            self::Warning, self::Failed => 'warning',
-            self::Error, self::Critical, self::Alert, self::Emergency => 'danger',
-            default => 'none',
-        };
+        $classes = [
+            self::Processed     => "success",
+            self::Debug         => "info",
+            self::Info          => "info",
+            self::Notice        => "info",
+            self::Processing    => "info",
+            self::Warning       => "warning",
+            self::Failed        => "warning",
+            self::Error         => "danger",
+            self::Critical      => "danger",
+            self::Alert         => "danger",
+            self::Emergency     => "danger",
+        ];
+
+        return $classes[$this->value] ?? "none";
+
     }
 
     public static function caseValues(): array
